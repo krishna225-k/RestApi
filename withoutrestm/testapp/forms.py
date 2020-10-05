7c0d1beb036678f7a66ffcb2225bb9023bb7b5f3
krishna225-k <53045047+krishna225-k@users.noreply.github.com>
2020-10-05 19:36:10 +0530

    Add files via upload

---
+++ withoutrestm/testapp/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from testapp.models import Employee
+
+
+# demonstration of ModelForm
+class EmployeeForm(forms.ModelForm):
+    '''
+    Blow lines was Implementing the validataion on form
+    '''
+
+    def clean_esal(self):
+        inputsal = self.cleaned_data['esal']
+        if inputsal < 5000:
+            raise forms.ValidationError('The mininum salary should be 5000')
+        return inputsal
+
+    class Meta:
+        model = Employee
+        fields = '__all__'
